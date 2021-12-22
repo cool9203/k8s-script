db1d649566b1cbe8886403e58e63bccd0d50b055
cool9203 <octer18@gmail.com>
2021-12-22 19:30:44 +0800

    commit all file

---
+++ install-cri.sh
@@ -0,0 +1,33 @@
+sudo apt-get install -y software-properties-common apt-transport-https ca-certificates curl
+
+## 如果以下這一行說只有root才能執行，那請輸入sudo su，然後輸入該指令，接著按下ctrl+d來退出root
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+### Add docker apt repository.
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+## Setup daemon.
+## Log 的大小, Storage Driver
+### 如果以下這一行說只有root才能執行，那請輸入sudo su，然後輸入該指令，接著按下ctrl+d來退出root
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+## Restart docker.
+sudo systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
---
+++ install-kube.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+#下載 Google Cloud 公開簽名密鑰:
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#添加 Kubernetes apt 倉庫：
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#更新 apt 包索引，安裝 kubelet、kubeadm 和 kubectl，並鎖定其版本：
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
